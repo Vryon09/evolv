7e9ec5a8926af7b14f7c855986047d3972f2d486
Vryon Antonio <vryonantonio@gmail.com>
2025-09-29 02:07:56 +0800

    feat: backend is working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,23 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = 5000;
+
+app.use(cors({ origin: ["http://localhost:5173"] }));
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log("Listening to PORT: " + PORT));
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("Test");
+  res.status(200).json({ message: "Test" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log("Listening to PORT: " + process.env.PORT)
+);
