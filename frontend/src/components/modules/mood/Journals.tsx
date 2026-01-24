36570d2123b3a7fa736b1147d52acb47931191b2
Vryon Antonio <vryonantonio@gmail.com>
2026-01-24 15:12:13 +0800

    feat: added back button in journals page

--- frontend/src/components/modules/mood/Journals.tsx
+++ frontend/src/components/modules/mood/Journals.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { handleGetJournals } from "@/services/apiJournals";
 import { useQuery } from "@tanstack/react-query";
+import { MoveLeftIcon } from "lucide-react";
+import { useNavigate } from "react-router";
 import type { IJournal } from "types/journal";
 
 function Journals() {
@@ -8,12 +11,21 @@ function Journals() {
     queryKey: ["journals"],
   });
 
+  const navigate = useNavigate();
+
   if (isLoading) return <p>Is loading...</p>;
 
   if (journals?.length === 0) return <p>No journal</p>;
 
   return (
     <div>
+      <Button
+        size="icon-lg"
+        className="cursor-pointer"
+        onClick={() => navigate("/app/mood")}
+      >
+        <MoveLeftIcon />
+      </Button>
       {journals?.map((journal, i) => (
         <div key={i}>
           <p>{journal.title}</p>
