c32569a856ee892e9a9380ef96590bd43070b0fe
Vryon Antonio <vryonantonio@gmail.com>
2026-03-14 10:12:23 +0800

    fix: finance pie charts falling back to the fallback

--- frontend/src/components/modules/finance/FinanceChart.tsx
+++ frontend/src/components/modules/finance/FinanceChart.tsx
@@ -1,59 +1,7 @@
 import { handleGetTransactionsChartStats } from "@/services/apiTransactions";
 import { useQuery } from "@tanstack/react-query";
 import IncomeExpenseBarChart from "./charts/IncomeExpenseBarChart";
-import type { ChartConfig } from "@/components/ui/chart";
 import CategoryPieChart from "./charts/CategoryPieChart";
-import { categories } from "@/constants/finance";
-
-const expenseChartConfig = {
-  Food: {
-    label: "Food",
-    color: "#22c55e", // green
-  },
-  Transport: {
-    label: "Transport",
-    color: "#3b82f6", // blue
-  },
-  Bills: {
-    label: "Bills",
-    color: "#ef4444", // red
-  },
-  Personal: {
-    label: "Personal",
-    color: "#a855f7", // purple
-  },
-  Leisure: {
-    label: "Leisure",
-    color: "#f59e0b", // amber
-  },
-  Savings: {
-    label: "Savings",
-    color: "#14b8a6", // teal
-  },
-  Misc: {
-    label: "Misc",
-    color: "#6b7280", // gray
-  },
-} satisfies ChartConfig;
-
-const incomeChartConfig = {
-  Primary: {
-    label: "Primary",
-    color: "#16a34a", // dark green
-  },
-  Side: {
-    label: "Side",
-    color: "#0ea5e9", // sky blue
-  },
-  Passive: {
-    label: "Passive",
-    color: "#8b5cf6", // violet
-  },
-  Other: {
-    label: "Other",
-    color: "#64748b", // slate
-  },
-} satisfies ChartConfig;
 
 function FinanceChart() {
   const { data: chartStats, isPending: isChartStatsPending } = useQuery({
@@ -61,24 +9,22 @@ function FinanceChart() {
     queryKey: ["chartStats"],
   });
 
+  if (isChartStatsPending) return <p>loading...</p>;
+
+  console.log(chartStats);
+
   return (
     <div>
       <IncomeExpenseBarChart />
       <div className="flex flex-col">
-        {!isChartStatsPending && (
-          <>
-            <CategoryPieChart
-              chartConfig={expenseChartConfig}
-              transactions={chartStats[0]?.categories}
-              categories={categories.Expense}
-            />
-            <CategoryPieChart
-              chartConfig={incomeChartConfig}
-              transactions={chartStats[1]?.categories}
-              categories={categories.Income}
-            />
-          </>
-        )}
+        <CategoryPieChart
+          transactions={chartStats[0]?.categories ?? []}
+          chartType={chartStats[0]?.transactionType ?? "Expense"}
+        />
+        <CategoryPieChart
+          transactions={chartStats[1]?.categories ?? []}
+          chartType={chartStats[1]?.transactionType ?? "Income"}
+        />
       </div>
     </div>
   );
--- frontend/src/components/modules/finance/charts/CategoryPieChart.tsx
+++ frontend/src/components/modules/finance/charts/CategoryPieChart.tsx
@@ -6,61 +6,85 @@ import {
   ChartTooltipContent,
   type ChartConfig,
 } from "@/components/ui/chart";
-import { useMemo } from "react";
-import { Pie, PieChart } from "recharts";
+import { Cell, Pie, PieChart } from "recharts";
 import type { ITransaction } from "types/Transaction";
 
-interface IChartData {
-  category: string;
-  amount: number;
-}
-
 function CategoryPieChart({
-  chartConfig,
   transactions,
-  categories,
+  chartType,
 }: {
-  chartConfig: ChartConfig;
   transactions: ITransaction[];
-  categories: string[];
+  chartType: "Income" | "Expense";
 }) {
-  const chartData = useMemo(() => {
-    const initialChartData: IChartData[] = categories.map((category) => {
-      return {
-        category,
-        amount: 0,
-        fill: chartConfig[category]?.color ?? "#ccc",
-      };
-    });
+  const expenseChartConfig = {
+    Food: {
+      label: "Food",
+      color: "#22c55e", // green
+    },
+    Transport: {
+      label: "Transport",
+      color: "#3b82f6", // blue
+    },
+    Bills: {
+      label: "Bills",
+      color: "#ef4444", // red
+    },
+    Personal: {
+      label: "Personal",
+      color: "#a855f7", // purple
+    },
+    Leisure: {
+      label: "Leisure",
+      color: "#f59e0b", // amber
+    },
+    Savings: {
+      label: "Savings",
+      color: "#14b8a6", // teal
+    },
+    Misc: {
+      label: "Misc",
+      color: "#6b7280", // gray
+    },
+  } satisfies ChartConfig;
 
-    return transactions.reduce((acc: IChartData[], curr: ITransaction) => {
-      const categoryId = acc.findIndex((a) => a.category === curr.category);
+  const incomeChartConfig = {
+    Primary: {
+      label: "Primary",
+      color: "#16a34a", // dark green
+    },
+    Side: {
+      label: "Side",
+      color: "#0ea5e9", // sky blue
+    },
+    Passive: {
+      label: "Passive",
+      color: "#8b5cf6", // violet
+    },
+    Other: {
+      label: "Other",
+      color: "#64748b", // slate
+    },
+  } satisfies ChartConfig;
 
-      if (categoryId === -1) return acc;
-
-      acc[categoryId].amount += curr.amount;
-
-      return acc;
-    }, initialChartData);
-  }, [categories, chartConfig, transactions]);
-
-  // const chartData = transactions
-  //   ? transactions.reduce((acc: IChartData[], curr: ITransaction) => {
-  //       const categoryId = acc.findIndex((a) => a.category === curr.category);
-
-  //       if (categoryId === -1) return acc;
-
-  //       acc[categoryId].amount += curr.amount;
-
-  //       return acc;
-  //     }, initialChartData)
-  //   : initialChartData;
+  const chartConfig =
+    chartType === "Income" ? incomeChartConfig : expenseChartConfig;
 
   return (
     <ChartContainer config={chartConfig} className="w-full">
       <PieChart>
         <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-        <Pie data={chartData} dataKey="amount" label nameKey="category" />
+        <Pie data={transactions} dataKey="amount" label nameKey="category">
+          {transactions?.map((entry, id) => (
+            <Cell
+              key={`${entry.category}-${id}`}
+              fill={
+                (chartConfig as Record<string, { color: string }>)[
+                  entry.category
+                ]?.color ?? "#8884d8"
+              }
+            />
+          ))}
+        </Pie>
         <ChartLegend content={<ChartLegendContent nameKey="category" />} />
       </PieChart>
     </ChartContainer>
